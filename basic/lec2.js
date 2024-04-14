65ae8a8265255628cdb2583383c9a0a5c7d24108
Nitin Upadhyay <113929330+Ni-3Upadhyay@users.noreply.github.com>
2024-04-14 08:05:02 +0000

    sd

---
+++ basic/lec2.js
@@ -0,0 +1,9 @@
+"use strict";                                       // is used so that your code must follow new standard.
+
+
+const accountId = 144453;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
